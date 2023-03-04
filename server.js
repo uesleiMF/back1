ff1dd4b37af2a638059399d61a339f5c055f99de
Ueslei <joseueslei2@gmail.com>
2023-03-04 18:08:37 -0300

    123

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path');
-const mongoose = require('mongoose');
-const routes =  require('./src/routes');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/curso-basico-mern',{
-    useUnifiedTopology:true,
-    useNewUrlParser:true,
-    useFindAndModify:false
-},function (err){
-    if(err){
-        console.log(err)
-    }else{
-        console.log('MongoDB CONECTADO com sucesso!')
-    }
-});
-
-app.use(cors());
-app.use(cookieParser());
+// Middlewares
 app.use(express.json());
-app.use(routes);
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", " "],
+    credentials: true,
+  })
+);
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.listen(port,function(){
-    console.log(`Server runing on port ${port}`)
+// Routes Middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
+
+// Error Middleware
+app.use(errorHandler);
+// Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
